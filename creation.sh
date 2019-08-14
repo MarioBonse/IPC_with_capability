df9bea944f5b5ef38f38878184ecf8d54db62441
mariobonse <mario.bonsembiante@gmail.com>
2019-08-14 11:07:22 +0200

    Added e bash script that compiles the module and run quemu. Updated also the .gitignore.

---
+++ creation.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# make
+CURR_DIRECTORY=$(pwd)
+MODULE_REL_PATH="module/module_test"
+KERNELVERSION=$(ls "$CURR_DIRECTORY/kernel")
+KERNELDIR="$CURR_DIRECTORY/kernel/$KERNELVERSION"
+
+# First move in the module directory and compile it
+cd $MODULE_REL_PATH
+make -C $KERNELDIR M=$(pwd)
+# eventually create the directory that contains the module
+cd $CURR_DIRECTORY
+mkdir Test
+cp $MODULE_REL_PATH/*.ko Test/
+find FS | cpio -o -H newc | gzip > mod.gz
+cat tinyfs.gz mod.gz > test.gz
+qemu-system-x86_64 -kernel "$KERNELDIR/arch/x86_64/boot/bzImage" -initrd test.gz
